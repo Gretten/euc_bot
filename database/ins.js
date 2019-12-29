ed84422964adf68340d034128b0401a7707a4660
Grett <shhworkmail@yandex.ru>
2019-12-29 14:57:54 +0000

    Добработана обратная связь между юзером и базой. Добавлены команды /add и /show для взаимодействия. Известные баги: падает, если ввести /show, если в коллекции ничего нет. Не удаляет кнопку на стадии добавления модели, если в марке меньше одной модели. Не поднимается, если не закомментить код после падения.

--- database/ins.js
+++ database/ins.js
@@ -1,16 +1,33 @@
 const sqlite = require('sqlite-sync');
 sqlite.connect('base.db');
 
-
-
-  sqlite.run(`INSERT INTO users (TG_ID, NICK, MODEL)
+/*
+sqlite.run(`INSERT INTO users (TG_ID, NICK, MODEL)
 	      VALUES (467173752, "@somerift", "Ninebot One S2")
 	  ;`, function(res){
     if(res.error)
         throw res.error;
-  })
+})
+*/
 
-sqlite.run("SELECT * FROM users;", function(res){
-    console.log(res);
- })
 
+
+
+sqlite.run(`CREATE TABLE IF NOT EXISTS users(
+   ID INTEGER PRIMARY KEY AUTOINCREMENT,
+   TG_ID INTEGER,
+   NICK TEXT,
+   MODEL TEXT NOT NULL
+);`, 
+function(res) {
+    if (res.error) {
+        throw res.error;
+        console.log(res);
+    }
+});
+
+
+/* sqlite.run(`DROP TABLE users;`, function(res) {
+	console.log(res);
+})
+*/
--- index.js
+++ index.js
@@ -22,34 +22,64 @@ const getWheelModels = (str) => {
 		} while (i >= 0);
 	})
 	if(str) {
-	    return new RegExp(array.join("|"), 'gi');
+	    return new RegExp(array.join("$|"), 'gi');
 	} else return array;
 };
 
-
-bot.command('garage', (ctx) => {
+bot.command('add', (ctx) => {
   return ctx.reply('Отлично, выбери марку колеса!', Extra.markup(
     Markup.keyboard(getWheelModels())
     .oneTime()
   ))
 })
 
-bot.hears(getWheelModels('str'), (ctx) => {
+bot.hears(/(Gotway$|Inmotion$|Kingsong$|Ninebot$|Airwheel$|Rockwheel$)/gi, (ctx) => {
     let markId = 0;
-    //console.log(ctx.message.text);
-    //sqlite.run(`SELECT * FROM euc_marks WHERE MARK = "${ctx.message.text}"`, (res) => console.log(res[0].ID));
-   
-    sqlite.run(`SELECT ID FROM euc_marks WHERE MARK = "${ctx.message.text}"`, (res) => markId = res[0].ID);	
-    console.log(markId);
-    sqlite.run(`SELECT * FROM euc_models WHERE MK_ID = ${markId}`, (res) => {
-        console.log(res)
-    })
+    let modelArray = []; 
     
+    // sqlite.run(`SELECT * FROM euc_marks WHERE MARK = "${ctx.message.text}"`, (res) => console.log(res[0].ID));
+     
+    sqlite.run(`SELECT ID FROM euc_marks WHERE MARK = "${ctx.message.text}"`, (res) => markId = res[0].ID);	
+    
+    sqlite.run(`SELECT * FROM euc_models WHERE MK_ID = ${markId}`, (res) => {
+         let i = res.length-1;
+         do {
+             modelArray[i] = res[i].MODEL_NAME;
+             i--;
+         } while (i >= 0);
+    })
+    return ctx.reply('Теперь выбери марку!', Extra.markup(
+    Markup.keyboard(modelArray)
+    .oneTime()
+  ))
+   
+     
 
 });
 
-/* api connection */
+bot.hears(/(Gotway|Inmotion|Kingsong|Ninebot|Airwheel|Rockwheel)/gi, (ctx) => {
+   sqlite.run(`INSERT INTO users(TG_ID, NICK, MODEL) VALUES (${ctx.from.id}, "${ctx.from.username}", "${ctx.message.text}")`)
+   return ctx.reply(`Отлично, @${ctx.from.username}, ${ctx.message.text} добавлен в твой гараж!`);
+});
 
-// bot.start(toStart);
+bot.command('user', (ctx) => {
+    return console.log(ctx.from);
+})
+
+bot.command('show', (ctx) => {
+    const user = ctx.from.username;
+    const id = ctx.from.id;
+    let userModels = '';
+    sqlite.run(`SELECT MODEL FROM users WHERE TG_ID = ${id}`, (res) => {
+    	let i = res.length-1;
+         do {
+             userModels += res[i].MODEL + '⚡️' + '\n';
+             i--;
+         } while (i >= 0);
+
+    })
+    return ctx.reply(`Мои модели:\n${userModels}`);
+})
+/* api connection */
 
 bot.launch();
